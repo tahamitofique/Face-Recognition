4341bea619669e89f1819a70ec51a0640002e085
TahamiTofique <tahamitofique@hotmail.com>
2019-12-11 23:52:15 +0500

    labels added

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ Promise.all([
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-]).then(startVideo)
+]).then(startVideo).then(start)
 
 function startVideo() {
   navigator.getUserMedia(
@@ -15,6 +15,9 @@ function startVideo() {
   )
 }
 
+async function start() {
+const labeledFaceDescriptors = await loadLabeledImages()
+const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
@@ -24,8 +27,25 @@ video.addEventListener('play', () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors()
     //document.body.append(detections.length)
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawDetections(canvas, resizedDetections, { label: results.toString() })
     //faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
   }, 100)
 })
+}
+function loadLabeledImages() {
+  const labels = ['tahami']
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = []
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(`https://github.com/tahamitofique/Face-Recognition/tree/master/labled%20images/${label}/${i}.jpg`)
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+        descriptions.push(detections.descriptor)
+      }
+
+      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    })
+  )
+}
