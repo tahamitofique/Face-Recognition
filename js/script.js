ba5740ee39ad05adb137d26db3b24d729f79f177
TahamiTofique <tahamitofique@hotmail.com>
2020-01-10 20:03:04 +0500

    new sample pictures

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ video.addEventListener('play', () => {
 }
 
 function loadLabeledImages() {
-  const labels = ['tahami','saim','taha'];
+  const labels = ['tahami','taha'];
   
   // const labels = ['saim'];
   // const proxy = `https://cors-anywhere.herokuapp.com/`;
@@ -49,7 +49,7 @@ function loadLabeledImages() {
     labels.map(async label => {
       const descriptions = []
       
-       for (let i = 1; i <= 1; i++) {
+       for (let i = 1; i <= 11; i++) {
         //https://raw.githubusercontent.com/tahamitofique/Face-Recognition/master/labled%20images/tahami/1.jpg
         const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/tahamitofique/Face-Recognition/master/labled%20images/${label}/${i}.jpg`, { mode: 'no-cors' });
         
